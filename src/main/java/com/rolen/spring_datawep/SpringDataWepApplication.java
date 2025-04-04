b382f139332353af7be2bcfe5ffffeac6b4da466
rolen <rolen@kakao.com>
2025-04-04 10:58:32 +0900

    First commit

---
+++ src/main/java/com/rolen/spring_datawep/SpringDataWepApplication.java
@@ -0,0 +1,13 @@
+package com.rolen.spring_datawep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataWepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rolen/spring_datawep/SpringDataWepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolen.spring_datawep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataWepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
