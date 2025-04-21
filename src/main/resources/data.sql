d89d685f3abe2b28589a53ce453f51540f103be3
rolen <rolen@kakao.com>
2025-04-21 23:30:50 +0900

    [INSERT] Article List

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content) VALUES ('타이틀 1', '콘텐트 1')
+INSERT INTO article (title, content) VALUES ('타이틀 2', '콘텐트 2')
+INSERT INTO article (title, content) VALUES ('타이틀 3', '콘텐트 3')
